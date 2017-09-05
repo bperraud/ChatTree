3fa80e896a3ee8a222195463857da1a622810e9e
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-09-05 10:47:09 -0400

    Add global models

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  email: string;
+  password: string;
+  confirmPassword: string;
+
+  constructor() {
+    this.email           = '';
+    this.password        = '';
+    this.confirmPassword = '';
+  }
+
+}
