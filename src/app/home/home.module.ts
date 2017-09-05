284a05c61d8b33ea91ca987afd495a9b268932e0
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-09-05 11:15:49 -0400

    Update homeModule with content

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,57 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ConversationsPanelComponent } from './conversations-panel/conversations-panel.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ThreadComponent } from './thread/thread.component';
+import { WebSocketService } from './_services/web-socket.service';
+import { ConversationService } from './_services/conversation.service';
+import { ThreadService } from './_services/thread.service';
+import { HomeEmptyComponent } from './home-empty/home-empty.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { SafeBase64ImgPipe } from './_pipes/safe-base64-img.pipe';
+import { UserNamePipe } from './_pipes/user-name.pipe';
+import { ToastService } from './_services/toast.service';
+import { Nl2BrPipe } from 'nl2br-pipe';
+import { NewMessageComponent } from './thread/new-message/new-message.component';
+import { LogoutService } from './_services/logout.service';
+//import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 @NgModule({
-  imports: [
-    CommonModule
+  declarations: [
+    HomeComponent,
+    HomeEmptyComponent,
+    ConversationComponent,
+    ConversationsPanelComponent,
+    ProfileComponent,
+    ThreadComponent,
+    NewMessageComponent,
+    SafeBase64ImgPipe,
+    UserNamePipe,
+    Nl2BrPipe
   ],
-  declarations: [HomeComponent]
+  imports: [
+    HomeRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    JsonpModule,
+    //SocketIoModule.forRoot({ url: 'http://localhost:3000', options: {} } as SocketIoConfig),
+    ModalModule.forRoot(),
+    TypeaheadModule.forRoot()
+  ],
+  providers   : [
+    LogoutService,
+    WebSocketService,
+    ConversationService,
+    ThreadService,
+    ToastService
+  ]
 })
-export class HomeModule { }
+export class HomeModule {}
